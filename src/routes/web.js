f108ab93a95ee1dd0107c4c709f13a25b6add6c4
dangngockhieu <dangngockhieu28082005@gmail.com>
2025-06-04 22:33:14 +0700

    Update CRUD

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const {getHomePage, getABC} = require('../controllers/homeController')
+const {getHomePage, postCreateUser,postUpdateUser, postDeleteUser, postHandleRemoveUser, getCreatePage, getUpdatePage} = require('../controllers/homeController')
 const router = express.Router()
 
 router.get('/', getHomePage)
-router.get('/abc', getABC)
+router.get('/create', getCreatePage)
+router.get('/update/:id', getUpdatePage)
+router.post('/create-user', postCreateUser)
+router.post('/update-user', postUpdateUser)
+router.post('/delete-user/:id', postDeleteUser)
+router.post('/delete-user', postHandleRemoveUser)
 module.exports = router
